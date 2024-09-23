5be8ccb44bbc96ae4ec60debfd7a117798fdbc3e
MayaMohann <mayamohan2212@gmail.com>
2024-09-23 20:17:51 +0530

    last-update

---
+++ src/Components/ParentComponent.jsx
@@ -0,0 +1,19 @@
+// // import React from 'react';
+// import { useSelector } from 'react-redux';
+// import CartItem from './cartItem';
+
+// const ParentComponent = () => {
+//   const cartItems = useSelector((state) => state.cart.items); // Get items from Redux store
+
+//   const handleContinueShopping = () => {
+//     console.log("Continue shopping");
+//   };
+
+//   return (
+//     <div>
+//       <CartItem items={cartItems} onContinueShopping={handleContinueShopping} />
+//     </div>
+//   );
+// };
+
+// export default ParentComponent;
